70ef166877343dcb7e3a9c8f38d57e49e87e16f4
constanza <constanzasofia.cerroaragon@gracia.lasalle.cat>
2024-03-04 12:30:46 +0100

    Avance

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        //Comentario
+        String[] personajes = {"Lobo", "Lobo", "Aldeano", "Aldeano", "Aldeano", "Vidente", "Cupido" };
+        ArrayList<Integer> idasignados
+
+
+
+        for (int i=0; i<personajes.length; i++){
+            personajes[i] = personajes [i]
+        }
+
+
+
+
+
+
+
+
+
 
     }
 }
