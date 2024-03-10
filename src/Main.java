cd94640ce3b819ef7fa2993c34793c9aa0747363
Satenik <saten914@gmail.com>
2024-03-10 19:52:49 +0100

    cambios de estructura

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,17 @@
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.List;
 
-public class Main {
-    public static Scanner input = new Scanner(System.in);
-    public static String[][] jugadadores = new String[7][2];
+public class  Main {
+    public static Scanner scanner = new Scanner(System.in);
+    public static Random random = new Random();
+
     public static void main(String[] args) {
-        String[] personaje = {"Lobo", "Lobo", "Aldeano", "Aldeano", "Aldeano", "Vidente", "Cupido"};
-        ArrayList<Integer> idsAsignados = new ArrayList<>();
-        Random random = new Random();
-        while (idsAsignados.size() < personaje.length) {
-            int id = random.nextInt(7) + 1;
-            if (!idsAsignados.contains(id)) {
-                idsAsignados.add(id);
-            }
-        }
-        for (int i = 0; i < personaje.length; i++) {
-            System.out.println(personaje[i] + " corresponde a id" + " " + idsAsignados.get(i));
-        }
+     Main juego=new Main(); //Creamos el objeto juego
+     juego.iniciarJuego();
     }
-    private static String pedirNombre () {
-        System.out.println("Ingresa tu nombre: ");
-        String Nombre = input.nextLine();
-        return Nombre;
+    public void iniciarJuego() {
+        System.out.println("Bienvenidos al juego.");
     }
-
-
 }
